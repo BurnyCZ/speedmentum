ca621767e3c4fa07c47ffce47bc9c5fce083bd9c
Burny <happymancz@email.cz>
2020-02-13 16:00:06 +0100

    Started working on movement switching menu

--- Speedmentum/Assets/Scripts/MovementMenu.cs
+++ Speedmentum/Assets/Scripts/MovementMenu.cs
@@ -1,22 +1,50 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.EventSystems;
 using UnityEngine.UI;
 
 public class MovementMenu : MonoBehaviour
 {
-    public Button button1;
-    public Button button2;
-    // Start is called before the first frame update
-    void Start()
-    {
-        button1.GetComponentInChildren<Text>().text = "Basic";
-        button2.GetComponentInChildren<Text>().text = "Faster";
-    }
+    //public Button basicButton;
+    //public KeyCode basicButtonKey = KeyCode.Alpha1;
+    //public Button increasingSpeedButton;
+    //public KeyCode increasingSpeedButtonKey = KeyCode.Alpha2;
+
+    
+
+    
+
+
+    
 
     // Update is called once per frame
     void Update()
     {
         
+        //if (Input.GetKeyDown(basicButtonKey)) //if changing mode button was pressed
+        //{
+
+        //    Graphic graphic = GetComponent<Graphic>();
+        //    graphic.CrossFadeColor(basicButton.colors.pressedColor, basicButton.colors.fadeDuration, true, true);
+        //    basicButton.onClick.Invoke();
+        //    Debug.Log("1");
+        //}
+        //else if(Input.GetKeyUp(basicButtonKey))
+        //{
+        //    Graphic graphic = GetComponent<Graphic>();
+        //    graphic.CrossFadeColor(basicButton.colors.normalColor, basicButton.colors.fadeDuration, true, true);
+        //}
+        //else if (Input.GetKeyDown(increasingSpeedButtonKey)) //if changing mode button was pressed
+        //{
+        //    increasingSpeedButton.onClick.Invoke();
+        //    Graphic graphic = GetComponent<Graphic>();
+        //    graphic.CrossFadeColor(increasingSpeedButton.colors.pressedColor, increasingSpeedButton.colors.fadeDuration, true, true);
+        //    Debug.Log("2");
+        //}
     }
+
+    
+
+    //ExecuteEvents.Execute(button.gameObject, new BaseEventData(eventSystem), ExecuteEvents.submitHandler);
 }
