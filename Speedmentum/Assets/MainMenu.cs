80083b10723b10081eac97337e575364cca59d43
Burny <happymancz@email.cz>
2020-02-13 02:11:24 +0100

    Added Menu

---
+++ Speedmentum/Assets/MainMenu.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void Level1()
+    {
+        SceneManager.LoadScene("Arena");
+    }
+    public void Level2()
+    {
+        SceneManager.LoadScene("CsMap");
+    }
+    public void Level3()
+    {
+        SceneManager.LoadScene("Playground");
+    }
+    public void Level4()
+    {
+        //SceneManager.LoadScene("Arena");
+    }
+    public void QuitGame()
+    {
+        Debug.Log("Quit");
+        Application.Quit();
+    }
+}
