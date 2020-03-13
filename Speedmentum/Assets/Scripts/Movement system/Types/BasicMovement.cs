45b1ae994bfd89e24fe40a2877a2a535cf3db3b0
Burny <happymancz@email.cz>
2020-03-13 03:38:19 +0100

    Update BasicMovement.cs
    Small changes to BasicMovement.cs

--- Speedmentum/Assets/Scripts/Movement system/Types/BasicMovement.cs
+++ Speedmentum/Assets/Scripts/Movement system/Types/BasicMovement.cs
@@ -26,6 +26,7 @@ public class BasicMovement : MonoBehaviour
     float x, z;
 
     public float growingValue = 1f;
+    public float decreasingValue = 1f;
 
     void Update()
     {
@@ -34,7 +35,10 @@ public class BasicMovement : MonoBehaviour
 
     public void Movement()
     {
-        gravity = -20f;
+
+        //type float value, default 100
+
+        gravity = -20f; //default gravity value, must be here because each update its set to that value first, and then depending on the combination of other gravity modifiers it adds or removes to that original value (might be better with multiple conditions? probs not) (might be better if the gravity was made as a slider instead)
         if (MovementModeController.enabledModes.Contains(Modes.LowGravity)) gravity += 10f;
         if (MovementModeController.enabledModes.Contains(Modes.HighGravity)) gravity += -10f;
 
@@ -51,32 +55,37 @@ public class BasicMovement : MonoBehaviour
 
         x = Input.GetAxis("Horizontal"); //getaxis function gives 1 if right key is pressed
             z = Input.GetAxis("Vertical"); //1 if forwards key is pressed
-                                                 //forward backwards sideways calculating
+                                           //forward backwards sideways calculating
 
-            /* first dumb idea - https://pastebin.com/CFXXtY8s*/
+        /* first dumb idea - https://pastebin.com/CFXXtY8s*/
 
 
-        
-        if (MovementModeController.enabledModes.Contains(Modes.IncreasingSpeed))
+        velocityXZ = (transform.right * x + transform.forward * z) * speed; //transform right is red arrow vector, from -1 to 1, forward is blue axis //forward backwards sideways direction
+                                                                            //those arrows are multiplied by x or z, which are 0 if nothing is pressed or 1 if wasd is pressed
+                                                                            //then its added together, forming a vector pointing to the direction where i wanna go
+                                                                            //and multiplied by the speed constant
+                                                                            //}
+        if (!MovementModeController.enabledModes.Contains(Modes.IncreasingSpeed) && !MovementModeController.enabledModes.Contains(Modes.DecreasingSpeed)) //if no increasing or decreasing spee
         {
-            growingValue = growingValue * 1.001f;
-            velocityXZ = (transform.right * x + transform.forward * z) * speed * growingValue;
-
-        }
-        else if (MovementModeController.enabledModes.Contains(Modes.DecreasingSpeed))
-        {
-            growingValue = growingValue * 0.999f;
-            velocityXZ = (transform.right * x + transform.forward * z) * speed * growingValue;
-
+            decreasingValue = 1;
+            growingValue = 1;
         }
         else
         {
-            velocityXZ = (transform.right * x + transform.forward * z) * speed; //transform right is red arrow vector, from -1 to 1, forward is blue axis //forward backwards sideways direction
-                                                                                //those arrows are multiplied by x or z, which are 0 if nothing is pressed or 1 if wasd is pressed
-                                                                                //then its added together, forming a vector pointing to the direction where i wanna go
-                                                                                //and multiplied by the speed constant
-                                                                                //}
-            growingValue = 1;
+            if (MovementModeController.enabledModes.Contains(Modes.IncreasingSpeed))
+            {
+                growingValue = growingValue * 1.001f;
+                velocityXZ = velocityXZ * growingValue;
+                if (!MovementModeController.enabledModes.Contains(Modes.DecreasingSpeed))
+                    decreasingValue = 1f;
+            }
+            if (MovementModeController.enabledModes.Contains(Modes.DecreasingSpeed))
+            {
+                decreasingValue = decreasingValue / 1.001f;
+                velocityXZ = velocityXZ * decreasingValue;
+                if (!MovementModeController.enabledModes.Contains(Modes.DecreasingSpeed))
+                    growingValue = 1f;
+            }
         }
 
         controller.Move(velocityXZ * Time.deltaTime); //forward backwards sideways
